010513a549b83af53ea7fc8c56ad90b1ce5b5c48
2101335173toyo <2101335173tz@cyber-u.ac.jp>
2023-12-06 11:21:43 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -2,35 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 
-// レスポンスのでーた（ノート全体）
-const responseObjectDataAll = {
-  textObject1 : {
-    id: 1,
-    title: 'ノート１のタイトルです',
-    subTitle: 'ノート１のサブタイトルです',
-    bodyText: 'ノート１の本文です'
-  },
-  textObject2 : {
-    id: 2,
-    title: 'ノート２のタイトルです',
-    subTitle: ' ノート２のサブタイトルです',
-    bodyText: 'ノート２本文です'
-  },
-};
 
-/**
- * メモを全件取得するAPI
- * @returns {Object[]} data
- * @returns {number} data.id - ID
- * @returns {string} data.title - タイトル
- * @returns {string} data.text - 内容
- */
+// 接続情報を設定
+const { MongoClient } = require("mongodb");
+const uri = "ac-dbqxgiw-shard-00-02.tts1afe.mongodb";
+const client = new MongoClient(uri);
 
-router.get('/', function (req, res, next) {
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
+const database = client.db('notes');
+const notes = database.collection('notes');
 
-// 全件取得して返す
+// idが１のドキュメントを取得
+const query = { id: 2 };
+const note = await notes.findOne(query);
 
-res.json(responseObjectDataAll);
+res.json(note);
 })
 
 module.exports = router;
